dd644c733f284a6ceef19bdbd77f3fea21639c6e
nukulele <zonough@gmail.com>
2022-06-06 11:06:47 +0400

    serve json

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log("Hello World");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
